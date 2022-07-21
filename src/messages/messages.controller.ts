7b958a713055f7943d6089ac3b385c64d4b2f058
putilinad98 <putilinad98@unet.univie.ac.at>
2022-07-21 15:26:30 +0300

    reorganize code

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,18 @@
+import {Body, Controller, Get, Post} from "@nestjs/common";
+import {CreateMessageDto} from "./dto/create-message.dto";
+
+@Controller("messages")
+export class MessagesController {
+    @Get()
+    listMessages() {
+    }
+
+    @Post()
+    createMessage(@Body() body: CreateMessageDto) {
+        console.log("createMessage: ", body);
+    }
+
+    @Get("/:id")
+    getMessage() {
+    }
+}
