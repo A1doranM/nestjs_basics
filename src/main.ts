59222685fed3f674589444ebb67ada3553433856
A1doranM <vaselisk1997@gmail.com>
2022-07-28 15:51:54 +0300

    New project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import {NestFactory} from "@nestjs/core";
 import {ValidationPipe} from "@nestjs/common";
 import {FastifyAdapter, NestFastifyApplication} from "@nestjs/platform-fastify";
-
-
+import {AppModule} from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(
-
+        AppModule,
         new FastifyAdapter()
     );
     app.useGlobalPipes(
